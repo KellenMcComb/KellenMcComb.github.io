05f12669ff4fb80598ae8d4d5cfdbea0fd4bae4b
John Wick <kmvisualstudio@hotmail.com>
2022-01-28 22:41:58 -0500

    created ICE 3

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,12 @@
 // IIFE -- Immediately Invoked Function Expression - (function(){})()
 // AKA anonymous self-executing function
+"use strict";
 (function()
 {
     function DisplayHome()
     {
+        console.log("Home Page");
+
         let AboutUsButton = document.getElementById("AboutUsButton");
         AboutUsButton.addEventListener("click", function()
         {
@@ -19,7 +22,7 @@
         let MainParagraph = document.createElement("p");
         let Article = document.createElement("article");
         let ArticleParagraph = `
-        <p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`
+        <p id="ArticleParagraph" class="mt-3">This is the Article Paragraph!</p>`
 
         // Step 3 - configure new element
         MainParagraph.setAttribute("id", "MainParagraph");
@@ -35,27 +38,86 @@
         Article.innerHTML = ArticleParagraph;
         DocumentBody.appendChild(Article);
         
-        // Example - replace the whole document body with super string!
-        // DocumentBody.innerHTML = `
-        //     <div class ="container">
-        //     <h1 class="display-1">Hello, World!</h1>
-        //     <p class="mt-5">and...what do you think of this!</p>
-        //     </div>
-        // `;
+    }
 
 
+    function DisplayAboutPage()
+    {
+        console.log("About Page");
+    }
 
 
-        // Example - insert DOM before
-        // MainContent.before(MainParagraph);
-
-        // Examples - deletion of AboutUsButton
-        // document.getElementById("AboutUsButton").remove();
-        // AboutUsButton.remove();
-
-        // ES6 and HTML5 - Template Strings - "Super Strings"
+    function DisplayProjectsPage()
+    {
+        console.log("Our Projects Page");
+    }
 
 
+    function DisplayServicesPage()
+    {
+        console.log("Our Services Page");
+    }
+
+
+    function DisplayContactPage()
+    {
+        console.log("Contact Us Page");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        sendButton.addEventListener("click", function(event)
+        {
+            if(subscribeCheckbox.checked)
+            {
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        console.log("Contact-List Page");
+        if(localStorage.length > 0)
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = ""; // data container - add deserialized data from the localStorage
+
+            let keys = Object.keys(localStorage); // returns a string array of keys
+
+            let index = 1; // counts number of keys
+
+            // for every key in the keys array (collection), loop
+            for (const key of keys) 
+            {
+                let contactData = localStorage.getItem(key); // get localStorage data value related to the key
+
+                let contact = new Contact(); // create empty Contact object
+                contact.deserialize(contactData);
+
+                // inject a repeatable row into the contactList table
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>
+                `;
+
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
     }
 
 
@@ -69,10 +131,28 @@
             case "Home":
                 DisplayHome();
                 break;
+
+            case "About Us":
+                DisplayAboutPage();
+                break;
+
+            case "Our Projects":
+                DisplayProjectsPage();
+                break;
+
+            case "Our Services":
+                DisplayServicesPage();
+                break;
+
+            case "Contact Us":
+                DisplayContactPage();
+                break;
+
+            case "Contact-List":
+                DisplayContactListPage();
+                break;
         }
     }
-
-
 
 
     window.addEventListener("load", Start);
